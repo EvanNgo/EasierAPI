7c795cc23b057fd1e3bc810885c356f203742c9d
EvanNgo <evanngo93@gmail.com>
2018-07-30 10:22:29 +0900

    add project

---
+++ CodeSQL.sql
@@ -0,0 +1,40 @@
+﻿USE [easi5810_easierjlpt]
+GO
+
+/****** Object: Table [dbo].[Users] Script Date: 27/07/2018 16:53:21 *****
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE [Users]
+
+CREATE TABLE [dbo].[Users] (
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
+    [UserName]    VARCHAR (25) NOT NULL,
+    [Email]       VARCHAR (50) NOT NULL,
+    [Birthyear]   INT          NOT NULL,
+    [HashPass]    VARCHAR (2083)  NULL,
+    [CreatedDate] DATE         NOT NULL,
+    [UpdatedDate] DATE         NOT NULL,
+    [EmailVerity] BIT          NOT NULL,
+	[Thumbnail] VARCHAR(2083)  NULL
+);
+
+CREATE TABLE [dbo].[Questions] (
+    [Id]          INT IDENTITY (1, 1) NOT NULL,
+    [UserId]      INT				NOT NULL,
+    [Title]       VARCHAR (255)		NOT NULL,
+	[Message]     VARCHAR (1000)	NULL,
+	[Thumbnail]   VARCHAR(2083)		NULL,
+	[Level]		  INT				NOT NULL,
+	[ColorId]     INT				NULL,
+    [CreatedDate] DATE				NOT NULL,
+);
+
+ALTER TABLE [dbo].[Users]  
+ADD CONSTRAINT PK_Users PRIMARY KEY CLUSTERED (Id);  
+
+ALTER TABLE [dbo].[Questions] ADD CONSTRAINT FK_User FOREIGN KEY(UserId) REFERENCES Users(Id);
+*/
---
+++ EasierAPI/Controllers/UsersController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Description;
+using EasierAPI.Models;
+
+namespace EasierAPI.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private easier_database db = new easier_database();
+
+        // GET: api/Users
+        public IQueryable<User> GetUsers()
+        {
+            return db.Users;
+        }
+
+        // GET: api/Users/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult GetUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            user.Questions = null;
+            return Ok(user);
+        }
+
+        // PUT: api/Users/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUser(int id, User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [Route("api/login")]
+        [HttpPost]
+        public ResponseMessageModels Login(Login login)
+        {
+            ResponseMessageModels result = new ResponseMessageModels();
+            String hash = getMd5Hash(login.Password);
+            String mEmail = login.Email + ".com";
+            User user = db.Users.FirstOrDefault(u => u.Email.Equals(mEmail) && u.HashPass.Equals(hash));
+            if (user == null)
+            {
+                result.status = 0;
+                result.message = "Invailid Email or Passsword";
+                result.data = null;
+                return result;
+            }
+            result.status = 1;
+            result.message = "Login Successfully";
+            UserInfo userInfo = new UserInfo(user);
+            result.data = userInfo;
+            return result;
+        }
+
+        // POST: api/Users
+        [ResponseType(typeof(User))]
+        public IHttpActionResult PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            user.HashPass = getMd5Hash(user.HashPass);
+            db.Users.Add(user);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = user.Id }, user);
+        }
+
+        // DELETE: api/Users/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult DeleteUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserExists(int id)
+        {
+            return db.Users.Count(e => e.Id == id) > 0;
+        }
+        private string getMd5Hash(string input)
+        { // Create a new instance of the MD5CryptoServiceProvider object.
+            MD5 md5Hasher = MD5.Create(); // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+            // Create a new Stringbuilder to collect the bytes // and create a string.
+            StringBuilder sBuilder = new StringBuilder(); // Loop through each byte of the hashed data // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+    }
+}
