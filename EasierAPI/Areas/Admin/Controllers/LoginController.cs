347287e506a12a8ad7bb858b92de2cbeceada648
EvanNgo <evanngo93@gmail.com>
2018-09-14 19:30:37 +0900

    test

--- EasierAPI/Areas/Admin/Controllers/LoginController.cs
+++ EasierAPI/Areas/Admin/Controllers/LoginController.cs
@@ -1,17 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using EasierAPI.Areas.API.Models;
 
 namespace EasierAPI.Areas.Admin.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Admin/Login
+        private easier_database db = new easier_database();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [MultiButton(MatchFormKey = "action", MatchFormValue = "Login")]
+        public ActionResult Login(LoginModel loginModel)
+        {
+            string hashpass = getMd5Hash(loginModel.Password);
+            User admin = db.Users.Where(u => u.Email == loginModel.Email && u.HashPass == hashpass).FirstOrDefault();
+            if (admin != null)
+            {
+                return View("UserLandingView");
+            }
+            else
+            {
+                ViewBag.Failedcount = "Login Failed";
+            }
+            return View("Index");
+        }
+
+        static string getMd5Hash(string input)
+        { // Create a new instance of the MD5CryptoServiceProvider object.
+            MD5 md5Hasher = MD5.Create(); // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+            // Create a new Stringbuilder to collect the bytes // and create a string.
+            StringBuilder sBuilder = new StringBuilder(); // Loop through each byte of the hashed data // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
         }
     }
 }
